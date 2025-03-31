e9d4660cac98b3b42f4cc00aa67c325837314c18
Anastasiia Tuhai <tuhaianastasia@gmail.com>
2025-03-31 21:50:52 +0300

    Added blank functions for reading and writing data from the console and files (input.py and output.py)

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def read_from_console():
+    pass
+
+
+def read_from_file_python(input_file_path):
+    pass
+
+
+def read_from_file_pandas(input_file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def write_to_console(output_text):
+    pass
+
+
+def write_to_file_python(output_file_path, output_text):
+    pass
