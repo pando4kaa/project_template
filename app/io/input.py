ccfa10ae5fa689c8ab94d485d71c976b2f288765
Anastasiia Tuhai <tuhaianastasia@gmail.com>
2025-03-31 21:58:24 +0300

    Added docstrings for functions in input.py and output.py

--- app/io/input.py
+++ app/io/input.py
@@ -1,10 +1,34 @@
 def read_from_console():
+    """
+    Read text from console.
+
+    Returns:
+        str: Text entered by user from console.
+    """
     pass
 
 
 def read_from_file_python(input_file_path):
+    """
+    Read text from file using built-in Python capabilities.
+
+    Args:
+        input_file_path (str): Path to the input file.
+
+    Returns:
+        str: Content of the file.
+    """
     pass
 
 
 def read_from_file_pandas(input_file_path):
+    """
+    Read data from file using pandas library.
+
+    Args:
+        input_file_path (str): Path to the input file.
+
+    Returns:
+        pandas.DataFrame: Data read from the file.
+    """
     pass
--- app/io/output.py
+++ app/io/output.py
@@ -1,6 +1,19 @@
 def write_to_console(output_text):
+    """
+    Write text to console.
+
+    Args:
+        output_text (str): Text to be written to console.
+    """
     pass
 
 
 def write_to_file_python(output_file_path, output_text):
+    """
+    Write text to file using built-in Python capabilities.
+
+    Args:
+        output_file_path (str): Path to the output file.
+        output_text (str): Text to be written to file.
+    """
     pass
